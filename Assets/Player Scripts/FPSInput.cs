aebcd2f9d1fc0bf59d4940128b21a54e1ee72a1f
Eddie Meza Jr <eddiemeza7@gmail.com>
2016-02-13 16:53:35 -0800

    push unity FPS alpha ver

---
+++ Assets/Player Scripts/FPSInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSInput : MonoBehaviour {
+
+	public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+
+		transform.Translate(deltaX,0,deltaZ);
+	}
+}
