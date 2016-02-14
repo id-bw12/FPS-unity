946b9d461063918ab007deff2de87a3069e89f6f
Eddie Meza Jr <eddiemeza7@gmail.com>
2016-02-13 21:04:56 -0800

    push "Half done with the basics"
    Can move and doesn't fly.

--- Assets/Player Scripts/FPSInput.cs
+++ Assets/Player Scripts/FPSInput.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class FPSInput : MonoBehaviour {
 
-	public float speed = 3.0f;
+	public float speed = 3.0f, gravity  = -9.8f ;
+	private CharacterController charController;
 
 	// Use this for initialization
 	void Start () {
 	
+		charController = GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +18,14 @@ public class FPSInput : MonoBehaviour {
 		float deltaX = Input.GetAxis ("Horizontal") * speed;
 		float deltaZ = Input.GetAxis ("Vertical") * speed;
 
-		transform.Translate(deltaX,0,deltaZ);
+		var movement = new Vector3 (deltaX, 0, deltaZ);
+
+		movement = Vector3.ClampMagnitude (movement, speed);
+		movement.y = gravity;
+
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection (movement);
+
+		charController.Move (movement);
 	}
 }
